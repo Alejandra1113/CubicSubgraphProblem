d7b150f9dc77d9fb168a56d8b7bbb6c8087bb9ec
Alejandra <amonzon1113@gmail,com>
2023-06-15 03:03:25 -0400

    Optimized Backtrack, Connex Graph Generator and Proof o theorem done in report

--- Code/Backtrack3Coloreable.py
+++ Code/Backtrack3Coloreable.py
@@ -10,8 +10,10 @@ def ThreeColorable(n, edges):
     set_B = []
     set_C = []
 
-    return ThreeColorableBacktrack(n,set_A,set_B,set_C, adjacent_matrix, 1)
+    # return ThreeColorableBacktrack(n,set_A,set_B,set_C, adjacent_matrix, 1)
+    return ThreeColorableB(n,set_A,set_B,set_C, adjacent_matrix, 1)
 
+#Backtrack que asigna colores y luego revis, sin podas
 def ThreeColorableBacktrack(n,set_A,set_B,set_C, adjacent_matrix, index):
     if index == n:
         return Void(set_A, adjacent_matrix) and Void(set_B, adjacent_matrix) and Void(set_C, adjacent_matrix)
@@ -27,6 +29,33 @@ def ThreeColorableBacktrack(n,set_A,set_B,set_C, adjacent_matrix, index):
     set_C.append(index)
     return ThreeColorableBacktrack(n, set_A, set_B, set_C, adjacent_matrix, index+1)
 
+#Backtrack que poda, no pone un vertice en un color donde tien vecinos
+def ThreeColorableB(n,set_A,set_B,set_C, adjacent_matrix, index):
+    if index == n:
+        return True
+    
+    C3 = None
+    
+    if FreeSet(set_A, index, adjacent_matrix): 
+        set_A.append(index)
+        C3 = ThreeColorableB(n, set_A, set_B, set_C, adjacent_matrix, index+1)
+        if C3: return True
+        set_A.pop()
+    
+    if FreeSet(set_B, index, adjacent_matrix): 
+        set_B.append(index)
+        C3 = ThreeColorableB(n, set_A, set_B, set_C, adjacent_matrix, index+1)
+        if C3: return True
+        set_B.pop()
+
+    if FreeSet(set_C, index, adjacent_matrix): 
+        set_C.append(index)
+        C3 = ThreeColorableB(n, set_A, set_B, set_C, adjacent_matrix, index+1)
+        if C3: return True
+
+    return False
+
+
 def Void(set_X, adjacent_matrix):
     for u in set_X:
         for v in set_X:
@@ -34,4 +63,6 @@ def Void(set_X, adjacent_matrix):
 
     return True
 
-
+def FreeSet(set_x, y, adjacent_matrix):
+    for u in set_X:
+        if  adjacent_matrix[u][y]: return False
